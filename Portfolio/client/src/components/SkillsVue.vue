8e7aaed832e0b3893f82f24aa099cdc51022244d
Gabriel Espinosa <54720@etu.he2b.be>
2024-11-04 23:18:17 +0100

    :add:(skills) add descirption for icon an link , need 'spoken language' view

--- Portfolio/client/src/components/SkillsVue.vue
+++ Portfolio/client/src/components/SkillsVue.vue
@@ -6,19 +6,20 @@
   <div class="container">
     <h1>
       <span class="colored">
-        {{$i18n.messages.msg.translation.content.skills.title}}
+        {{ $i18n.messages.msg.translation.content.skills.title }}
       </span>
     </h1>
     <div class="container_languages">
       <h2>
-        {{$i18n.messages.msg.translation.content.skills.title_languages}}
+        {{ $i18n.messages.msg.translation.content.skills.title_languages }}
       </h2>
       <h6>
         <ul>
           <div
             v-for="content in $i18n.messages.msg.translation.content.skills.spoken_languages"
             :key="content">
-            {{content.name}} - {{content.level}} <flag :iso="content.icon" />
+              <span>{{ content.name }}</span> <span> - </span> <span>{{ content.level }}</span>
+            <flag :iso="content.icon" />
           </div>
         </ul>
       </h6>
@@ -26,36 +27,56 @@
 
     <div class="container_tools">
       <h2>
-        {{$i18n.messages.msg.translation.content.skills.title_tools}}
+        {{ $i18n.messages.msg.translation.content.skills.title_tools }}
       </h2>
       <h6>
         <ul>
-          <div>
-            {{$i18n.messages.msg.translation.content.skills.cat_professional}}
-            <li
-              v-for="content in $i18n.messages.msg.translation.content.skills.icon_professional"
-            :key="content">
-              <a href=""
-                 :content="content.about"
-                 v-tippy="{
-                   arrow : true,
-                    arrowType : 'round',
-                    animation : 'fade',
-                    theme:'dark',
-                    interactive:true}">
-                <img
-                  :src="content.url"
-                  :alt="content.name"
-                  >
-              </a>
+          <div class="container_icon">
+            <div>
+              {{ $i18n.messages.msg.translation.content.skills.cat_professional }}
+            </div>
+            <div class="icons">
+              <div
+                v-for="content in $i18n.messages.msg.translation.content.skills.icon_professional"
+                :key="content"
+              >
+                <a :href="content.url" :title="content.about">
+                  <img :src="content.image" :alt="content.name">
+                </a>
+              </div>
+            </div>
+          </div>
 
-            </li>
+          <div class="container_icon">
+            <div>{{ $i18n.messages.msg.translation.content.skills.cat_school }}
+            </div>
+            <div class="icons">
+              <div
+                v-for="content in $i18n.messages.msg.translation.content.skills.icon_school"
+                :key="content"
+              >
+                <a :href="content.url" :title="content.about">
+                  <img :src="content.image" :alt="content.name">
+                </a>
+              </div>
+            </div>
           </div>
-          <div>
-            {{$i18n.messages.msg.translation.content.skills.cat_school}}
-          </div>
-          <div>
-            {{$i18n.messages.msg.translation.content.skills.cat_personal}}
+
+          <div class="container_icon">
+            <div>
+              {{ $i18n.messages.msg.translation.content.skills.cat_personal }}
+            </div>
+
+            <div class="icons">
+              <div
+                v-for="content in $i18n.messages.msg.translation.content.skills.icon_personal"
+                :key="content"
+              >
+                <a :href="content.url" :title="content.about">
+                  <img :src="content.url" :alt="content.name">
+                </a>
+              </div>
+            </div>
           </div>
         </ul>
       </h6>
@@ -65,7 +86,31 @@
 </template>
 
 <style scoped>
-  img{
-    width: 50px;
-  }
+.container {
+  text-align: left;
+  display: grid;
+  margin: 3vh;
+  align-self: center;
+}
+
+.container_languages {
+  margin-top: 2vh;
+  margin-bottom: 3vh;
+
+}
+.container_icon {
+  display: grid;
+}
+
+.icons {
+  margin: 1vh;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+img {
+  width: 40px;
+  margin: 1vh;
+}
+
 </style>
